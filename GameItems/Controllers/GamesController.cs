5d2a9b7f5f98d33cf40060c62fd4cfeb27d63421
logotie <phelpsakinsan@gmail.com>
2019-07-05 22:32:29 +0100

    Add project files.

---
+++ GameItems/Controllers/GamesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameItems.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        // GET games/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+        
+    }
+}
