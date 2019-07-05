dca20dc573296a1616f221054df754bdf445ad9a
logotie <phelpsakinsan@gmail.com>
2019-07-05 23:17:05 +0100

    Added model classes

---
+++ GameItems/Model/Games/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameItems.Model.Games
+{
+    public class Comment
+    {
+        public string user { get; set; }
+        public string message { get; set; }
+        public string dateCreated { get; set; }
+        public int like { get; set; }
+    }
+}
