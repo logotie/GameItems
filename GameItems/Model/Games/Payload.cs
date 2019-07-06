6f7f1b2c335d50c6bc9a4b4a60f1f47b100b6acb
logotie <phelpsakinsan@gmail.com>
2019-07-06 14:04:09 +0100

    Added Payload model class

--- GameItems/Model/Games/Payload.cs
+++ GameItems/Model/Games/Payload.cs
@@ -5,6 +5,20 @@ using System.Threading.Tasks;
 
 namespace GameItems.Model.Games
 {
+    ///<summary> 
+    ///Model class for the Payload JSON
+    ///{
+    ///"title":"Uncharted 4",
+    ///"description":"For the first time ever in Uncharted history",
+    ///"by":"Sony",
+    ///"platform":["PS4"],
+    ///"age_rating":"16",
+    ///"likes":100,
+    ///"comments":
+    ///[{"user":"bob","message":"Cracking game far too much cinematic","dateCreated":"2011-01-03","like":6}
+    ///]
+    ///}
+    /// </summary>
     public class Payload
     {
     }
