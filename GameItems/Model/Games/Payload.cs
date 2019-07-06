127ba622bded3eb32c82ab646f3a91afd5d0c6a0
logotie <phelpsakinsan@gmail.com>
2019-07-06 14:06:42 +0100

    Added variables for payload class

--- GameItems/Model/Games/Payload.cs
+++ GameItems/Model/Games/Payload.cs
@@ -15,11 +15,19 @@ namespace GameItems.Model.Games
     ///"age_rating":"16",
     ///"likes":100,
     ///"comments":
-    ///[{"user":"bob","message":"Cracking game far too much cinematic","dateCreated":"2011-01-03","like":6}
+    ///[
+    ///{"user":"bob","message":"Cracking game far too much cinematic","dateCreated":"2011-01-03","like":6}
     ///]
     ///}
     /// </summary>
     public class Payload
     {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string by { get; set; }
+        public string[] platform { get; set; }
+        public string age_rating { get; set; }
+        public int likes { get; set; }
+        public Comment[] comment { get; set; }
     }
 }
