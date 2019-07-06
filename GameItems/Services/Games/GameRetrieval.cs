6778468c612fb09f309304f764cda835ff72530a
logotie <phelpsakinsan@gmail.com>
2019-07-06 15:20:26 +0100

    Added DI for the service

--- GameItems/Services/Games/GameRetrieval.cs
+++ GameItems/Services/Games/GameRetrieval.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameItems.Model;
 using GameItems.Model.Games;
 
 namespace GameItems.Services.Games
 {
     public class GameRetrieval : IGameRetrieval
     {
+        private readonly GameItemsContext gameItemsContext;
+
+        public GameRetrieval(GameItemsContext _gameItemsContext)
+        {
+            gameItemsContext = _gameItemsContext;
+        }
+
         public Game RetrieveGameById(int gameId)
         {
             throw new NotImplementedException();
--- GameItems/Startup.cs
+++ GameItems/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GameItems.Model;
+using GameItems.Services.Games;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -30,6 +31,8 @@ namespace GameItems
             services.AddDbContext<GameItemsContext>(opt => opt.UseInMemoryDatabase("GameItems"));
             //Specifies also that it will use an in memory database
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //Dependency Injection for the GameRetrieval service, also handles injection of the Db Context
+            services.AddScoped<IGameRetrieval, GameRetrieval>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
