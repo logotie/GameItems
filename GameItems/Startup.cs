d57b7ce52e44ca736d77a6ffe1d2306c3b8cba57
logotie <phelpsakinsan@gmail.com>
2019-07-06 15:13:09 +0100

    Added db context for injection

--- GameItems/Startup.cs
+++ GameItems/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameItems.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,6 +26,9 @@ namespace GameItems
         //Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Register the service with the Dependency Injection Container, DbContext is a service
+            services.AddDbContext<GameItemsContext>(opt => opt.UseInMemoryDatabase("GameItems"));
+            //Specifies also that it will use an in memory database
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
