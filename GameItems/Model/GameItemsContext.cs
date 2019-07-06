fe036a03760c57ca9eae4252f170290ab19250f3
logotie <phelpsakinsan@gmail.com>
2019-07-06 15:12:10 +0100

    Added db context

---
+++ GameItems/Model/GameItemsContext.cs
@@ -0,0 +1,24 @@
+﻿using GameItems.Model.Games;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameItems.Model
+{
+    /// <summary>
+    /// Primary class responsible for handling db operations
+    /// </summary>
+    public class GameItemsContext: DbContext
+    {
+        public GameItemsContext(DbContextOptions<GameItemsContext> options) : base(options)
+        {
+
+        }
+
+        //DbSet collection of Game objects, similar to a table and rows.
+        public DbSet<Game> Games { get; set; }
+
+    }
+}
--- GameItemsTest/GamesControllerTests.cs
+++ GameItemsTest/GamesControllerTests.cs
@@ -5,10 +5,16 @@ namespace GameItemsTest
 {
     public class GamesControllerTests
     {
+        //Test that if no id nothing comes back
         [Fact]
         public void Test1()
         {
 
         }
+
+        //Test that if letters are sent nothing comes back
+
+
+        //Test that if an id is sent something valid comes back
     }
 }
