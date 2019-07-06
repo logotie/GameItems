95a1b65e381969b8e74e77f1d1b9ee540925078e
logotie <phelpsakinsan@gmail.com>
2019-07-06 15:17:38 +0100

    Added Game retrieval service

---
+++ GameItems/Services/Games/GameRetrieval.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameItems.Model.Games;
+
+namespace GameItems.Services.Games
+{
+    public class GameRetrieval : IGameRetrieval
+    {
+        public Game RetrieveGameById(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GameItems/Services/Games/IGameRetrieval.cs
@@ -0,0 +1,13 @@
+﻿using GameItems.Model.Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameItems.Services.Games
+{
+    public interface IGameRetrieval
+    {
+        Game RetrieveGameById(int gameId);
+    }
+}
