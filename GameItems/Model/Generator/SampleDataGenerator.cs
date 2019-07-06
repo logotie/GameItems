a3bb9acbb67fe33accb1bc169a2d36aa8764294a
logotie <phelpsakinsan@gmail.com>
2019-07-06 17:34:36 +0100

    Changed resources to public and added sample game data generator

--- GameItems/Model/Generator/SampleDataGenerator.cs
+++ GameItems/Model/Generator/SampleDataGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using GameItems.Model.Games;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,12 @@ namespace GameItems.Model.Generator
 {
     public class SampleDataGenerator
     {
+
         public Game GenerateSampleGameData()
         {
-
+            String sampleGamesJson = GameItems.Resource.SampleJsonData;
+            Game sampleData = JsonConvert.DeserializeObject<Game>(sampleGamesJson);
+            return sampleData;
         }
     }
 }
