6f75e9cb1ffd4cb4e516c08b3e88b54f7e36edbd
logotie <phelpsakinsan@gmail.com>
2019-07-06 17:29:41 +0100

    Added JSON.Net

---
+++ GameItems/Model/Generator/SampleDataGenerator.cs
@@ -0,0 +1,16 @@
+﻿using GameItems.Model.Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameItems.Model.Generator
+{
+    public class SampleDataGenerator
+    {
+        public Game GenerateSampleGameData()
+        {
+
+        }
+    }
+}
